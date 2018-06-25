f1cc8e99954aeb0abadc4f1fd80869c2a129dacf
disanshijie <878455649@qq.com>
2018-06-25 23:59:05 +0800

    gtrh

---
+++ src/main/java/com/oracle/sun/main/StartBuy.java
@@ -0,0 +1,12 @@
+package com.oracle.sun.main;
+
+import com.oracle.sun.core.FileUps;
+
+public class StartBuy {
+
+	public static void main(String[] args) {
+		//getPath();
+		
+		FileUps.startUp(2);
+	}
+}
---
+++ src/main/java/com/oracle/sun/main/StartPick.java
@@ -0,0 +1,12 @@
+package com.oracle.sun.main;
+
+import com.oracle.sun.core.FileUps;
+
+public class StartPick {
+
+	public static void main(String[] args) {
+		//getPath();
+		
+		FileUps.startUp(3);
+	}
+}
